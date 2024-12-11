6d3a449bc56bf1c1fed1723634ba1e4d2a910fee
Diasb4 <133123959+Diasb4@users.noreply.github.com>
2024-12-11 11:04:25 +0500

    Add files via upload
    Assignment 1

---
+++ Main.java
@@ -0,0 +1,131 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Assignment 1.1
+        //Task 1
+        //String name;
+        //int age;
+        //String city;
+        //Task 2
+        //int a = 9;
+        //int b = 10;
+        //Task 3
+        //int year = 3126;
+        //System.out.println(year - 8);
+        //Task 4
+        //String word = "AITU";
+        //String Phrase = "I love Tom and Jerry";
+        //String Line = "-----";
+        //String text = "I hope i will return my scholarship very soon";
+
+        //Assignment 1.2
+        //Task 1
+        Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt();
+        //if (n < 0) {
+        //    System.out.println("it’s cold outside");
+        //} else {
+        //    System.out.println("it's warm outside");
+        //}
+        //Task2
+        //String name = sc.nextLine();
+        //int age = sc.nextInt();
+        //if (age >= 18 && age <= 28) {
+        //    System.out.println(name + "," + "come to the military registration and enlistment office");
+        //}
+        //Task 3
+        //int temperature = sc.nextInt();
+        //boolean IsHigh = temperature > 37;
+        //boolean IsLow = temperature < 36;
+        //if (!IsHigh && !IsLow) {
+        //    System.out.println("Normal");
+        //}else System.out.println(IsHigh ? "High" : "Low");
+        //Task 4
+        //int age = sc.nextInt();
+        //if (age < 20 || age > 60) {
+        //    System.out.println("You dont have to work");
+        //}
+        //if (age > 20 && age < 60) {;
+        //}
+        //Task 5
+        //String s = "I will never work for penni";
+        //int n = 0;
+        //while (n < 100)
+        //{
+        //    System.out.println(s);
+        //    n++;
+        //}
+        //Task 6
+        //int m = 0;
+//        while (sc.hasNext()) {
+//            if (sc.hasNextInt())
+//            {
+//                int n = sc.nextInt();
+//                 m += n;
+//            }else
+//            {
+//                String input = sc.next();
+//                if (input.equalsIgnoreCase("ENTER"))
+//            {
+//                break;
+//            }
+//            }
+//        }
+//        System.out.println(m);
+        //Task 7
+//        int n = 0;
+//        while (n < 5) {
+//        int m = 0;
+//         while (m < 10) {
+//          System.out.print("Q");
+//          m++;
+//         }
+//         System.out.println();
+//        n++;
+//            }
+        //Task 8
+//        int sum = 0;
+//        int n = 0;
+//        while (n <= 100){
+//            if (n % 3 ==0){
+//                n++;
+//                continue;
+//            }
+//            sum += n;
+//            n++;
+//        }
+//            System.out.println(sum);
+        //Task 9
+//        for (int i=1; i < 15; i++)
+//        {
+//            if (i % 2 == 0){
+//                System.out.println(i);
+//            }
+//        }
+        //Task 10
+//        int start = sc.nextInt();
+//        int end = sc.nextInt();
+//        int multi = sc.nextInt();
+//        int sum = 0;
+//        for (int i = start; i < end; i++) {
+//            if (i % multi != 0) {
+//                continue; // Skip numbers that are not multiples of the given number
+//            }
+//            sum += i;
+//        }
+//        System.out.println(sum);
+        //Task 11
+//        for(int i = 1; i <= 10; i++) {
+//            for(int j = 1; j <= i; j++) {
+//                System.out.print("8");
+//            }
+//            System.out.println();
+//        }
+
+
+
+    }
+}
